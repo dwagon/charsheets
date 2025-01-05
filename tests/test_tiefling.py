6add7f51a7ea8d8ec6e478953387f1d321b697b5
Dougal Scott <dougal.scott@gmail.com>
2025-01-05 18:20:11 +1100

    Tieflings

---
+++ tests/test_tiefling.py
@@ -0,0 +1,106 @@
+import unittest
+
+from charsheets.constants import Skill, DamageType
+from charsheets.species import Tiefling, Legacy
+from charsheets.spells import Spells
+from tests.dummy import DummyCharClass, DummyOrigin
+
+
+#######################################################################
+class TestTieflingAbyssal(unittest.TestCase):
+    def setUp(self):
+        self.c = DummyCharClass(
+            "test_tiefling",
+            DummyOrigin(),
+            Tiefling(Legacy.ABYSSAL),
+            Skill.DECEPTION,
+            Skill.PERCEPTION,
+            strength=16,
+            dexterity=14,
+            constitution=15,
+            intelligence=10,
+            wisdom=10,
+            charisma=12,
+        )
+
+    ###################################################################
+    def test_speed(self):
+        self.assertEqual(self.c.speed.value, 30)
+
+    ###################################################################
+    def test_dmg_resistance(self):
+        self.assertIn(DamageType.POISON, self.c.damage_resistances)
+
+    ###################################################################
+    def test_prepared_spells(self):
+        self.assertIn(Spells.POISON_SPRAY, self.c.known_spells)
+        self.assertIn(Spells.THAUMATURGY, self.c.known_spells)
+
+
+#######################################################################
+class TestTieflingChthonic(unittest.TestCase):
+    def setUp(self):
+        self.c = DummyCharClass(
+            "test_tiefling",
+            DummyOrigin(),
+            Tiefling(Legacy.CHTHONIC),
+            Skill.DECEPTION,
+            Skill.PERCEPTION,
+            strength=16,
+            dexterity=14,
+            constitution=15,
+            intelligence=10,
+            wisdom=10,
+            charisma=12,
+        )
+
+    ###################################################################
+    def test_speed(self):
+        self.assertEqual(self.c.speed.value, 30)
+
+    ###################################################################
+    def test_dmg_resistance(self):
+        self.assertIn(DamageType.NECROTIC, self.c.damage_resistances)
+
+    ###################################################################
+    def test_prepared_spells(self):
+        self.assertIn(Spells.CHILL_TOUCH, self.c.known_spells)
+        self.assertIn(Spells.THAUMATURGY, self.c.known_spells)
+
+
+#######################################################################
+class TestTieflingInfernal(unittest.TestCase):
+    def setUp(self):
+        self.c = DummyCharClass(
+            "test_tiefling",
+            DummyOrigin(),
+            Tiefling(Legacy.INFERNAL),
+            Skill.DECEPTION,
+            Skill.PERCEPTION,
+            strength=16,
+            dexterity=14,
+            constitution=15,
+            intelligence=10,
+            wisdom=10,
+            charisma=12,
+        )
+
+    ###################################################################
+    def test_speed(self):
+        self.assertEqual(self.c.speed.value, 30)
+
+    ###################################################################
+    def test_dmg_resistance(self):
+        self.assertIn(DamageType.FIRE, self.c.damage_resistances)
+
+    ###################################################################
+    def test_prepared_spells(self):
+        self.assertIn(Spells.FIRE_BOLT, self.c.known_spells)
+        self.assertIn(Spells.THAUMATURGY, self.c.known_spells)
+
+
+#######################################################################
+if __name__ == "__main__":  # pragma: no coverage
+    unittest.main()
+
+# EOF
