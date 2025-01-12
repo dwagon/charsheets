1b17ee5a6e5320439706ffc772ae02a626dc829f
Dougal Scott <dougal.scott@gmail.com>
2025-01-12 12:21:53 +1100

    Merge hunter and champion into class abilities

--- charsheets/abilities/__init__.py
+++ charsheets/abilities/__init__.py
@@ -1,33 +1,16 @@
-from charsheets.abilities.wizard import (
-    RitualAdept,
-    ArcaneWard,
-    ArcaneRecovery,
-    Scholar,
-    AbjurationSavant,
-    DivinationSavant,
-    Portent,
-    EvocationSavant,
-    PotentCantrip,
-    IllusionSavant,
-    ImprovedIllusions,
-    MemorizeSpell,
-)
-from charsheets.abilities.druid import (
-    Druidic,
-    WildShape,
-    WildCompanion,
-    LandsAid,
-    LandSpellArid,
-    LandSpellTemperate,
-    LandSpellTropical,
-    LandSpellPolar,
-    CircleForms,
-    WrathOfTheSea,
-    StarryForm,
-    StarMap,
-    Warden,
-    Magician,
-    WildResurgence,
+from charsheets.abilities.barbarian import (
+    Rage,
+    UnarmoredDefenseBarbarian,
+    DangerSense,
+    RecklessAttack,
+    PrimalKnowledge,
+    Frenzy,
+    AnimalSpeaker,
+    RageOfTheWilds,
+    VitalityOfTheTree,
+    DivineFury,
+    WarriorOfTheGods,
+    FastMovement,
 )
 from charsheets.abilities.cleric import (
     ChannelDivinity,
@@ -47,19 +30,23 @@ from charsheets.abilities.cleric import (
     TrickeryDomainSpells,
     SearUndead,
 )
-from charsheets.abilities.general import Darkvision120, Darkvision60, ExtraAttack, WeaponMastery
-from charsheets.abilities.ranger import (
-    FavoredEnemy,
-    DeftExplorer,
-    PrimalCompanion,
-    DreadfulStrikes,
-    OtherworldlyGlamour,
-    FeywildGifts,
-    DreadAmbusher,
-    UmbralSight,
+from charsheets.abilities.druid import (
+    Druidic,
+    WildShape,
+    WildCompanion,
+    LandsAid,
+    LandSpellArid,
+    LandSpellTemperate,
+    LandSpellTropical,
+    LandSpellPolar,
+    CircleForms,
+    WrathOfTheSea,
+    StarryForm,
+    StarMap,
+    Warden,
+    Magician,
+    WildResurgence,
 )
-from charsheets.abilities.hunter import HuntersLore, HuntersPrey
-from charsheets.abilities.champion import ImprovedCritical, RemarkableAthlete
 from charsheets.abilities.fighter import (
     FightingStyle,
     SecondWind,
@@ -70,54 +57,10 @@ from charsheets.abilities.fighter import (
     WarBond,
     PsionicPowerFighter,
     TacticalShift,
+    ImprovedCritical,
+    RemarkableAthlete,
 )
-from charsheets.abilities.warlock import (
-    EldritchInvocation,
-    PactMagic,
-    MagicalCunning,
-    StepsOfTheFey,
-    HealingLight,
-    DarkOnesBlessing,
-    PsychicSpells,
-)
-from charsheets.abilities.barbarian import (
-    Rage,
-    UnarmoredDefenseBarbarian,
-    DangerSense,
-    RecklessAttack,
-    PrimalKnowledge,
-    Frenzy,
-    AnimalSpeaker,
-    RageOfTheWilds,
-    VitalityOfTheTree,
-    DivineFury,
-    WarriorOfTheGods,
-    FastMovement,
-)
-from charsheets.abilities.paladin import (
-    LayOnHands,
-    PaladinsSmite,
-    SacredWeapon,
-    PeerlessAthlete,
-    NaturesWrath,
-    VowOfEmnity,
-    InspiringSmite,
-    FaithfulSteed,
-)
-from charsheets.abilities.rogue import (
-    Expertise,
-    SneakAttack,
-    ThievesCant,
-    CunningAction,
-    SteadyAim,
-    MageHandLegerdemain,
-    Assassinate,
-    AssassinsTools,
-    PsionicPowerRogue,
-    PsychicBlades,
-    FastHands,
-    SecondStoryWork,
-)
+from charsheets.abilities.general import Darkvision120, Darkvision60, ExtraAttack, WeaponMastery
 from charsheets.abilities.monk import (
     MartialArts,
     MonksFocus,
@@ -135,6 +78,42 @@ from charsheets.abilities.monk import (
     SlowFall,
     ImplementsOfMercy,
 )
+from charsheets.abilities.paladin import (
+    LayOnHands,
+    PaladinsSmite,
+    SacredWeapon,
+    PeerlessAthlete,
+    NaturesWrath,
+    VowOfEmnity,
+    InspiringSmite,
+    FaithfulSteed,
+)
+from charsheets.abilities.ranger import (
+    FavoredEnemy,
+    DeftExplorer,
+    PrimalCompanion,
+    DreadfulStrikes,
+    OtherworldlyGlamour,
+    FeywildGifts,
+    DreadAmbusher,
+    UmbralSight,
+    HuntersLore,
+    HuntersPrey,
+)
+from charsheets.abilities.rogue import (
+    Expertise,
+    SneakAttack,
+    ThievesCant,
+    CunningAction,
+    SteadyAim,
+    MageHandLegerdemain,
+    Assassinate,
+    AssassinsTools,
+    PsionicPowerRogue,
+    PsychicBlades,
+    FastHands,
+    SecondStoryWork,
+)
 from charsheets.abilities.sorcerer import (
     AberrantSorcery,
     ClockworkSpells,
@@ -147,4 +126,27 @@ from charsheets.abilities.sorcerer import (
     WildMagicSurge,
     TidesOfChaos,
     TelepathicSpeech,
+)
+from charsheets.abilities.warlock import (
+    EldritchInvocation,
+    PactMagic,
+    MagicalCunning,
+    StepsOfTheFey,
+    HealingLight,
+    DarkOnesBlessing,
+    PsychicSpells,
+)
+from charsheets.abilities.wizard import (
+    RitualAdept,
+    ArcaneWard,
+    ArcaneRecovery,
+    Scholar,
+    AbjurationSavant,
+    DivinationSavant,
+    Portent,
+    EvocationSavant,
+    PotentCantrip,
+    IllusionSavant,
+    ImprovedIllusions,
+    MemorizeSpell,
 )
--- charsheets/abilities/ranger.py
+++ charsheets/abilities/ranger.py
@@ -69,4 +69,27 @@ class UmbralSight(BaseAbility):
     Invisible condition to any creture that relies on Darkvision to see you in that Darkness."""
 
 
+#############################################################################
+class HuntersPrey(BaseAbility):
+    tag = Ability.HUNTERS_PREY
+    _desc = """You gain one of the following. Whenever you finish a Long Rest you can replace the option.
+
+    Colossus Slayer. Your tenacity can wear down even the most resilient foes. When you hit a creature with a weapon,
+    the weapon deals an extra 1d8 damage to the target if its missing any of its Hit Points. You can deal this extra 
+    damage only once per turn.
+
+    Horde Breaker. Once on each of your turns when you make an attack with a weapon, you can make another attack with 
+    the same weapon against a different creature that is within 5 feet of the original target, that is within the 
+    weapon's range, and you haven't attacked this turn.
+    """
+
+
+#############################################################################
+class HuntersLore(BaseAbility):
+    tag = Ability.HUNTERS_LORE
+    _desc = """You can call on the forces of nature to reveal certain strengths and weaknesses of your prey.
+    While a creature is marked by your Hunter’s Mark, you know whether that creature has any
+    Immunities, Resistances, or Vulnerabilities, and if the creature has any, you know what they are."""
+
+
 # EOF
