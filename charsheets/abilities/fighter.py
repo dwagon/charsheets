5860abdc216e0461e49d2f965705c9e021ab30c4
Dougal Scott <dougal.scott@gmail.com>
2025-01-18 20:50:59 +1100

    Eldritch Knight tuning

--- charsheets/abilities/fighter.py
+++ charsheets/abilities/fighter.py
@@ -23,13 +23,20 @@ class FightingStyle(BaseAbility):
 #############################################################################
 class SecondWind(BaseAbility):
     tag = Ability.SECOND_WIND
-    goes = 2
     _desc = """You have a limited well of physical and mental stamina that you can draw on. As a Bonus Action,
     you can use it to regain Hit Points equal to 1d10 plus your Fighter Level.
 
     You can use this feature twice. You regain one expended use when you finish a Short Rest, and you regain all
     expended uses when you finish a Long Rest.
     """
+
+    @property
+    def goes(self) -> int:
+        if self.owner.level >= 10:
+            return 4
+        elif self.owner.level >= 4:
+            return 3
+        return 2
 
 
 ############################################################################
