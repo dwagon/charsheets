1e47511f0d62b359a4eb678a358cf582fcee40f6
Dougal Scott <dougal.scott@gmail.com>
2025-01-29 11:57:04 +1100

    Monk examples

---
+++ examples/monk_shadow.py
@@ -0,0 +1,34 @@
+from charsheets.armour import Unarmoured
+from charsheets.classes import MonkWarriorOfShadow
+from charsheets.constants import Skill, Stat, Language
+from charsheets.features import AbilityScoreImprovement
+from charsheets.origins import Merchant
+from charsheets.species import Elf, Lineages
+
+character = MonkWarriorOfShadow(
+    "Xadina",
+    Merchant(Stat.INTELLIGENCE, Stat.CONSTITUTION, Stat.CHARISMA),
+    Elf(Lineages.WOOD_ELF, Skill.PERCEPTION),
+    Skill.HISTORY,
+    Skill.RELIGION,
+    strength=12,
+    dexterity=15,
+    constitution=13,
+    intelligence=10,
+    wisdom=14,
+    charisma=8,
+)
+
+character.player_name = "Phi"
+character.extras = {"hair": "bald", "alignment": "LG"}
+character.level2(hp=5)
+character.level3(hp=6)
+character.level4(hp=7, feat=AbilityScoreImprovement(Stat.DEXTERITY, Stat.DEXTERITY))
+character.level5(hp=6)
+character.level6(hp=5)
+character.level7(hp=4)
+
+
+character.wear_armour(Unarmoured())
+character.add_languages(Language.ELVISH, Language.PRIMORDIAL)
+character.add_equipment("Packed Lunch", "Ointment")
