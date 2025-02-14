1f22c5e818f171764549b45caef1b8f25d74e7ef
Dougal Scott <dougal.scott@gmail.com>
2025-02-14 17:48:37 +1100

    More warlock examples

---
+++ examples/thiir_warlock_archfey.py
@@ -0,0 +1,60 @@
+from charsheets.classes import WarlockArchFey
+from charsheets.classes.warlock import AgonizingBlast, PactOfTheBlade, EldritchSpear, EldritchSmite, ThirstingBlade, Lifedrinker
+from charsheets.constants import Skill, Stat, Language
+from charsheets.origins import Hermit
+from charsheets.species import Orc
+from charsheets.spell import Spell
+from charsheets.features import AbilityScoreImprovement
+
+character = WarlockArchFey(
+    "Thiir",
+    Hermit(Stat.CONSTITUTION, Stat.CHARISMA, Stat.CHARISMA),
+    Orc(),
+    Skill.DECEPTION,
+    Skill.INTIMIDATION,
+    strength=8,
+    dexterity=14,
+    constitution=13,
+    intelligence=12,
+    wisdom=10,
+    charisma=15,
+)
+character.player_name = "Delta"
+character.level1()
+character.add_invocation(PactOfTheBlade())
+
+character.level2(hp=4)
+character.add_invocation(AgonizingBlast(Spell.ELDRITCH_BLAST))
+character.add_invocation(EldritchSpear(Spell.ELDRITCH_BLAST))
+
+character.level3(hp=6)
+character.level4(hp=3, feat=AbilityScoreImprovement(Stat.CHARISMA, Stat.CHARISMA))
+character.level5(hp=6)
+character.add_invocation(EldritchSmite())
+character.add_invocation(ThirstingBlade())
+
+
+character.level6(hp=4)
+character.level7(hp=4)
+character.level8(hp=3, feat=AbilityScoreImprovement(Stat.CHARISMA, Stat.CHARISMA))
+character.level9(hp=4)
+character.add_invocation(Lifedrinker())
+
+
+character.add_languages(Language.GOBLIN, Language.GIANT)
+character.extras = {"hair": "bald", "alignment": "CE", "image": "characters/images/nende.png"}
+character.add_equipment("Stuff", "More Stuff", "Something Else")
+
+character.learn_spell(
+    Spell.ELDRITCH_BLAST,
+    Spell.PRESTIGITATION,
+    Spell.DETECT_THOUGHTS,
+    Spell.DISSONANT_WHISPERS,
+    Spell.PHANTASMAL_FORCE,
+    Spell.ARMOR_OF_AGATHYS,
+    Spell.HEX,
+    Spell.INVISIBILITY,
+    Spell.MISTY_STEP,
+    Spell.SUGGESTION,
+    Spell.TASHAS_HIDEOUS_LAUGHTER,
+)
---
+++ examples/thiir_warlock_fiend.py
@@ -0,0 +1,58 @@
+from charsheets.classes import WarlockFiend
+from charsheets.classes.warlock import AgonizingBlast, ArmorOfShadows, GiftOfTheDepths
+from charsheets.constants import Skill, Stat, Language
+from charsheets.origins import Acolyte
+from charsheets.species import Orc
+from charsheets.spell import Spell
+from charsheets.features import AbilityScoreImprovement, MagicInitiateCleric
+
+character = WarlockFiend(
+    "Thiir",
+    Acolyte(
+        Stat.CHARISMA,
+        Stat.CHARISMA,
+        Stat.CHARISMA,
+        MagicInitiateCleric(Stat.CHARISMA, Spell.SPARE_THE_DYING, Spell.THAUMATURGY, Spell.CURE_WOUNDS),
+    ),
+    Orc(),
+    Skill.DECEPTION,
+    Skill.INTIMIDATION,
+    strength=8,
+    dexterity=14,
+    constitution=13,
+    intelligence=12,
+    wisdom=10,
+    charisma=15,
+)
+character.player_name = "Delta"
+character.level1()
+character.level2(hp=4)
+character.level3(hp=6)
+character.level4(hp=3, feat=AbilityScoreImprovement(Stat.CHARISMA, Stat.CHARISMA))
+character.level5(hp=6)
+character.level6(hp=4)
+character.level7(hp=4)
+character.level8(hp=3, feat=AbilityScoreImprovement(Stat.CHARISMA, Stat.CHARISMA))
+character.level9(hp=4)
+
+
+character.add_languages(Language.GOBLIN, Language.UNDERCOMMON)
+character.extras = {"hair": "bald", "alignment": "CE", "image": "characters/images/nende.png"}
+character.add_equipment("Stuff", "More Stuff", "Something Else")
+character.add_invocation(AgonizingBlast(Spell.ELDRITCH_BLAST))
+character.add_invocation(ArmorOfShadows())
+character.add_invocation(GiftOfTheDepths())
+
+character.learn_spell(
+    Spell.ELDRITCH_BLAST,
+    Spell.PRESTIGITATION,
+    Spell.DETECT_THOUGHTS,
+    Spell.DISSONANT_WHISPERS,
+    Spell.PHANTASMAL_FORCE,
+    Spell.ARMOR_OF_AGATHYS,
+    Spell.HEX,
+    Spell.INVISIBILITY,
+    Spell.MISTY_STEP,
+    Spell.SUGGESTION,
+    Spell.TASHAS_HIDEOUS_LAUGHTER,
+)
