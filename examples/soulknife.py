ffa5f334bc38a955c9c222c747dccb04ca56882a
Dougal Scott <dougal.scott@gmail.com>
2025-04-03 11:41:36 +1100

    Soulknife

--- examples/soulknife.py
+++ examples/soulknife.py
@@ -1,19 +1,18 @@
 from charsheets.armour import Studded
-from charsheets.classes import RogueSoulknife
+from charsheets.character import Character
+from charsheets.classes import RogueSoulknife, Rogue
 from charsheets.constants import Skill, Stat, Language, DamageType
 from charsheets.features import AbilityScoreImprovement, Expertise, ElementalAdept
 from charsheets.origins import Entertainer
 from charsheets.species import Orc
 from charsheets.weapons import Rapier, Shortbow
 
-character = RogueSoulknife(
+character = Character(
     "Nolam the Bastard",
     Entertainer(Stat.DEXTERITY, Stat.DEXTERITY, Stat.CHARISMA),
     Orc(),
-    Skill.SLEIGHT_OF_HAND,
-    Skill.STEALTH,
-    Skill.PERCEPTION,
-    Skill.PERSUASION,
+    Language.DWARVISH,
+    Language.ORC,
     strength=12,
     dexterity=15,
     constitution=13,
@@ -23,21 +22,26 @@ character = RogueSoulknife(
 )
 character.extras = {"alignment": "CN", "image": "characters/images/aaliyah.jpg"}
 character.player_name = "Phi"
-character.level1(language=Language.DEEP_SPEECH, expertise=Expertise(Skill.STEALTH, Skill.HISTORY))
-character.level2(hp=5)
-character.level3(hp=6)
-character.level4(hp=5, feat=AbilityScoreImprovement(Stat.DEXTERITY, Stat.CHARISMA))
-character.level5(hp=6)
-character.level6(hp=4, expertise=Expertise(Skill.ACROBATICS, Skill.SLEIGHT_OF_HAND))
-character.level7(hp=4)
-character.level8(hp=5, feat=ElementalAdept(DamageType.FIRE, Stat.CHARISMA))
-character.level9(hp=4)
-character.level10(hp=6, feat=AbilityScoreImprovement(Stat.DEXTERITY, Stat.CONSTITUTION))
-character.level11(hp=8)
-character.level12(hp=6, feat=AbilityScoreImprovement(Stat.DEXTERITY, Stat.CONSTITUTION))
-character.level13(hp=5)
+character.add_level(
+    Rogue(
+        skills=[Skill.SLEIGHT_OF_HAND, Skill.STEALTH, Skill.PERCEPTION, Skill.PERSUASION],
+        language=Language.DEEP_SPEECH,
+        expertise=Expertise(Skill.STEALTH, Skill.HISTORY),
+    )
+)
+character.add_level(Rogue(hp=5))
+character.add_level(RogueSoulknife(hp=6))
+character.add_level(RogueSoulknife(hp=5, feat=AbilityScoreImprovement(Stat.DEXTERITY, Stat.CHARISMA)))
+character.add_level(RogueSoulknife(hp=6))
+character.add_level(RogueSoulknife(hp=4, expertise=Expertise(Skill.ACROBATICS, Skill.SLEIGHT_OF_HAND)))
+character.add_level(RogueSoulknife(hp=4))
+character.add_level(RogueSoulknife(hp=5, feat=ElementalAdept(DamageType.FIRE, Stat.CHARISMA)))
+character.add_level(RogueSoulknife(hp=4))
+character.add_level(RogueSoulknife(hp=6, feat=AbilityScoreImprovement(Stat.DEXTERITY, Stat.CONSTITUTION)))
+character.add_level(RogueSoulknife(hp=8))
+character.add_level(RogueSoulknife(hp=6, feat=AbilityScoreImprovement(Stat.DEXTERITY, Stat.CONSTITUTION)))
+character.add_level(RogueSoulknife(hp=5))
 
-character.add_languages(Language.DWARVISH, Language.ORC)
 character.add_weapon(Rapier(atk_bonus=1, dmg_bonus=1, name="Pointy End"))
 character.add_weapon(Shortbow())
 character.wear_armour(Studded(ac_bonus=1, name="Glamour"))
