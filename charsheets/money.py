aedc327fde52b97d40e87269ac74249b9bb3bd29
Dougal Scott <dougal.scott@gmail.com>
2026-01-09 10:46:11 +1100

    Remove Money class as wasn't adding value.

--- charsheets/money.py
+++ charsheets/money.py
@@ -14,26 +14,7 @@ class Coin(StrEnum):
     COPPER = auto()
 
 
-#############################################################################
-class Money:
-    """Handle money for a character"""
-
-    def __init__(self) -> None:
-        self._coins: dict[Coin, int] = {Coin.PLATINUM: 0, Coin.GOLD: 0, Coin.ELECTRUM: 0, Coin.SILVER: 0, Coin.COPPER: 0}
-
-    def add_coins(self, coin: Coin, amount: int) -> None:
-        """Add coins"""
-        self._coins[coin] += amount
-
-    def remove_coins(self, coin: Coin, amount: int) -> None:
-        """Remove coins"""
-        self._coins[coin] -= amount
-
-    def __getitem__(self, coin: Coin) -> int:
-        return self._coins[coin]
-
-
-MONEY = Money()
+MONEY: dict[Coin, int] = {Coin.PLATINUM: 0, Coin.GOLD: 0, Coin.ELECTRUM: 0, Coin.SILVER: 0, Coin.COPPER: 0}
 
 
 #############################################################################
@@ -41,14 +22,16 @@ class MoneyMixin:
     """Mixin to help reduce class bloat in Character"""
 
     #########################################################################
-    def add_coins(self, coin: Coin, amount: int):
+    @staticmethod
+    def add_coins(coin: Coin, amount: int):
         """Add coins"""
-        MONEY.add_coins(coin, amount)
+        MONEY[coin] += amount
 
     #########################################################################
-    def remove_coins(self, coin: Coin, amount: int):
+    @staticmethod
+    def remove_coins(coin: Coin, amount: int):
         """Remove / Spend coins"""
-        MONEY.remove_coins(coin, amount)
+        MONEY[coin] -= amount
 
     #########################################################################
     @property
