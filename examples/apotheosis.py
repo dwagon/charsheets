3ccae84819b3acb2dd00b391522a44d4f6cc96d2
Dougal Scott <dougal.scott@gmail.com>
2026-03-20 11:05:04 +1100

    Fix spells

--- examples/apotheosis.py
+++ examples/apotheosis.py
@@ -22,10 +22,62 @@ character = Character(
     charisma=10,
 )
 
+BACKGROUND = """Offspring of a forbidden dalliance between a Male Drow sell-sword adventurer (Chaszonim Yauntorzza)
+    and a Female High-Elf weapon maker (Lythienne Eilfina). He has a younger brother (Folmon), from a different father
+    who became a Law Keeper.
+
+    Apotheosis was never really accepted into the highly snobbish High Elf society, so hid away from them,
+    eventually hooking up with a criminal gang, using his burgeoning magical abilities to assist in burglaries and such.
+
+    When the heat got too much (and after an emotional showdown with Folmon), Apotheosis headed into the town of
+    Fenloe to make his way in the World, throw off the reach of the Law, and maybe also find his long missing Father."""
+
+character.extras = {
+    "character_background": BACKGROUND,
+    "eyes": "Pale red",
+    "skin": "Ashen grey",
+    "hair": "White gold",
+    "player_name": "Dougal",
+    "alignment": "CE",
+    "ideals": "Fuck society and their moral structures.",
+    "image": "characters/images/apotheosis.png",
+}
+
+# Level 1
 character.add_level(Wizard(skills=[Skill.ARCANA, Skill.INVESTIGATION]))
-character.learn_spell(Spell.FIRE_BOLT, Spell.MAGE_HAND, Spell.MESSAGE, Spell.BLADE_WARD)  # Cantrip
+character.learn_spell(Spell.FIRE_BOLT, Spell.MAGE_HAND, Spell.BLADE_WARD)  # Cantrip
 character.learn_spell(Spell.MAGIC_MISSILE, Spell.SHIELD, Spell.TASHAS_HIDEOUS_LAUGHTER, Spell.IDENTIFY)  # Lvl 1 Spells
-character.prepare_spells(Spell.MAGIC_MISSILE, Spell.SHIELD)
+character.prepare_spells(Spell.MAGIC_MISSILE, Spell.SHIELD, Spell.TASHAS_HIDEOUS_LAUGHTER, Spell.IDENTIFY)
+
+character.add_weapon(Quarterstaff())
+character.add_weapon(Shortbow())
+
+# Level 2
+character.add_level(Wizard(hp=5, scholar=Scholar(Skill.ARCANA)))
+character.learn_spell(Spell.BURNING_HANDS, Spell.ALARM)
+character.prepare_spells(Spell.BURNING_HANDS)
+
+# Level 3
+character.add_level(WizardAbjurer(hp=3))
+character.learn_spell(Spell.ARCANE_LOCK, Spell.ARCANE_VIGOR)
+character.unprepare_spells(Spell.IDENTIFY)
+character.prepare_spells(Spell.ARCANE_LOCK, Spell.ARCANE_VIGOR)
+character.prepare_spells(Spell.LIGHT)  # Boon from the Temple Statue
+
+character.add_coins(Coin.GOLD, 726)
+character.remove_coins(Coin.GOLD, 50)  # Bought healing potions
+character.remove_coins(Coin.GOLD, 10)  # Tome of body bits
+character.add_coins(Coin.GOLD, 160)  # Looting Aboleth temple
+character.add_coins(Coin.SILVER, 100)
+character.add_coins(Coin.COPPER, 34)
+
+# Level 4
+character.add_level(WizardAbjurer(hp=3, feat=AbilityScoreImprovement(Stat.INTELLIGENCE, Stat.INTELLIGENCE)))
+character.learn_spell(Spell.RAY_OF_FROST)  # Cantrip
+character.unprepare_spells(Spell.TASHAS_HIDEOUS_LAUGHTER)
+character.learn_spell(Spell.KNOCK, Spell.PHANTASMAL_FORCE)
+character.prepare_spells(Spell.KNOCK, Spell.PHANTASMAL_FORCE)
+
 character.add_equipment(
     "Dice Set",
     "Bedroll",
@@ -45,45 +97,3 @@ character.add_equipment(
     "Tome of Harvesting Monsters",
     "Burning Chicken Head Hat" "Aboleth Statue (Dominate Monster once) worth 200gp" "5 pieces of jewellery @ 50gp",
 )
-character.add_weapon(Quarterstaff())
-character.add_weapon(Shortbow())
-
-character.add_level(Wizard(hp=5, scholar=Scholar(Skill.ARCANA)))
-character.learn_spell(Spell.BURNING_HANDS)
-character.prepare_spells(Spell.BURNING_HANDS)
-
-character.add_level(WizardAbjurer(hp=3))
-character.learn_spell(Spell.KNOCK, Spell.ARCANE_LOCK, Spell.ARCANE_VIGOR)
-character.prepare_spells(Spell.KNOCK, Spell.ARCANE_LOCK)
-character.prepare_spells(Spell.LIGHT)  # Boon from the Temple Statue
-
-BACKGROUND = """Offspring of a forbidden dalliance between a Male Drow sell-sword adventurer (Chaszonim Yauntorzza)
-    and a Female High-Elf weapon maker (Lythienne Eilfina). He has a younger brother (Folmon), from a different father
-    who became a Law Keeper.
-    
-    Apotheosis was never really accepted into the highly snobbish High Elf society, so hid away from them,
-    eventually hooking up with a criminal gang, using his burgeoning magical abilities to assist in burglaries and such.
-    
-    When the heat got too much (and after an emotional showdown with Folmon), Apotheosis headed into the town of
-    Fenloe to make his way in the World, throw off the reach of the Law, and maybe also find his long missing Father."""
-
-character.extras = {
-    "character_background": BACKGROUND,
-    "eyes": "Pale red",
-    "skin": "Ashen grey",
-    "hair": "White gold",
-    "player_name": "Dougal",
-    "alignment": "CE",
-    "ideals": "Fuck society and their moral structures.",
-    "image": "characters/images/apotheosis.png",
-}
-character.add_coins(Coin.GOLD, 726)
-character.remove_coins(Coin.GOLD, 50)  # Bought healing potions
-character.remove_coins(Coin.GOLD, 10)  # Tome of body bits
-character.add_coins(Coin.GOLD, 160)  # Looting
-character.add_coins(Coin.SILVER, 100)
-character.add_coins(Coin.COPPER, 34)
-
-character.add_level(WizardAbjurer(hp=3, feat=AbilityScoreImprovement(Stat.INTELLIGENCE, Stat.INTELLIGENCE)))
-character.prepare_spells(Spell.RAY_OF_FROST)
-character.prepare_spells(Spell.PHANTASMAL_FORCE)
